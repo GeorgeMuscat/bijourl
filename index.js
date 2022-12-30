6255df2bfb639d049c99c8f583e8a95a4e0aad68
George Muscat <georgemuscat@proton.me>
2022-12-31 06:25:15 +1100

    hope this works

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Dependencies
 const express = require('express');
-
+require('dotenv').config();
 // Configure & Run the http server
 const app = express();
 
 app.use(express.static(__dirname, { dotfiles: 'allow' } ));
 
-app.listen(80, () => {
+app.listen(process.env.PORT, () => {
   console.log('HTTP server running on port 80');
 });
